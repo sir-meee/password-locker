0c0eb48914bbee5984a745aaa69c4e664d02bf13
sir-meee <sammymbevi@gmail.com>
2018-11-04 22:45:51 +0300

    Add tearDown method and multiple saves for users test

--- test.py
+++ test.py
@@ -11,7 +11,11 @@ from user import User
         Set up method to run before each test case.
         """
         self.new_user = User ("Sammy","Mutua") #create user object
-        
+         def tearDown(self):
+        """
+        tearDown method that does the clean-up after each test has run
+        """ 
+        User.user_list = []
      def test_init(self):
         """
         test_init test case to test if the object is initialized properly
@@ -23,5 +27,13 @@ from user import User
         """
         self.new_user.save_user() #save user
         self.assertEqual(len(User.user_list),1)
+        def test_save_multiple_user(self):
+        """
+        To check if we can save multiple objects into list
+        """
+        self.new_user.save_user()
+        test_user = User ("Test","34huhuhuhu")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
  if __name__ == '__main__':
     unittest.main() 
