c62cd50be82d29064613dd1d08b0a87e9f6bf23f
sir-meee <sammymbevi@gmail.com>
2018-11-04 20:50:48 +0300

    Create user_list variable to store created user objects

--- user.py
+++ user.py
@@ -2,6 +2,7 @@ class User:
     """
     Class that generates new instances of users.
     """
+     user_list = [] #Empty user list
      def __init__(self,username,password):
         """
         __init__ method that defines properties for our objects
