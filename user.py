35b3b88ab4eace277ceac113be49ab007dda0335
sir-meee <sammymbevi@gmail.com>
2018-11-04 20:49:21 +0300

    create and define user class

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    """
+    Class that generates new instances of users.
+    """
+     def __init__(self,username,password):
+        """
+        __init__ method that defines properties for our objects
+         Args:
+            username: New user username.
+            password: New user password.
+        """
+         self.username = username
+        self.password = password
