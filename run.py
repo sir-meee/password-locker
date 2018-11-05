881529234a931f8351826130b2991b6bf41dc8c8
sir-meee <sammymbevi@gmail.com>
2018-11-05 19:44:46 +0300

    Adds function to generate password, find, check and display credentials

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.6
+import string
+from random import choice
 from user import User , Credentials 
 def create_user(username,password):
     '''
@@ -32,3 +34,33 @@ def create_user(username,password):
     Function to delete a credential
     '''
     credentials.delete_credentials()
+def generate_password(self):
+    """
+    Function to generate random password
+    """
+    return password
+ def find_credentials(acc_name):
+    '''
+    Function that finds a credential by acc_name and returns the credential
+    '''
+    return Credentials.find_by_acc_name(acc_name)
+ def check_existing_contacts(number):
+    '''
+    Function that check if a credential exists with that acc_name and return a Boolean
+    '''
+    return Credentials.credentials_exist(number)
+ def display_credentials():
+    '''
+    Function that returns all the saved contacts
+    '''
+    return Credentials.display_credentials()
+ # def copy_credentials_login_name(acc_name):
+#     '''
+#     Function that copies login name to clipboard
+#     '''
+#     return Credentials.copy_login_name(acc_name)
+ def main():
+    print("WELCOME TO PASSWORD LOCKER. ")
+print('\n')
+ if __name__ == '__main__':
+    main()
